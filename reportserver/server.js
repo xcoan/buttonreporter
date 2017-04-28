61c31ed7ceab713db407c69a59767d8d736b2289
X Coan <xjcoan@gmail.com>
2017-04-28 13:46:40 -0400

    crime report API v1

---
+++ reportserver/server.js
@@ -0,0 +1,46 @@
+// Declare global variables
+const express = require('express')
+const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient
+
+const app = express()
+
+// EJS for views
+app.set('view engine', 'ejs')
+
+//body-parser for mongo
+app.use(bodyParser.urlencoded({extended: true}))
+
+// public folder
+app.use(express.static('public'))
+
+var db
+// Connect to MLAB mongoDB instance
+MongoClient.connect(`mongodb://dbuser:dbpass@ds131320.mlab.com:31320/learn-mongo-db`, (err, database) => {
+  if (err) return console.log(err)
+  db = database
+  app.listen(3000, () => {
+    console.log('listening on 3000')
+  })
+})
+
+
+app.get('/', (req, res) => {
+  db.collection('crimes').find().toArray((err, result) => {
+    if (err) return console.log(err)
+    res.render('index.ejs', {crimestamps: result})
+  })
+})
+
+// Creates new quote document with given name and quote
+app.post('/crime/:source', (req, res) => {
+  // Current time for timestamp
+  var date = new Date()
+  var src = req.params.source
+  db.collection('crimes').save({ timestamp: date.toISOString(), source: src}, (err, result) => {
+    if (err) return console.log(err)
+
+    res.send(result)
+    console.log(result)
+  })
+})
